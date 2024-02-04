92b76b367afa54abe57d9d9903eb00a1e37b847a
yuliabalkiss <balkisula451@gmail.com>
2024-02-05 01:03:40 +0300

    fixed bug in file index.js

--- index.js
+++ index.js
@@ -6,14 +6,20 @@ const startButton = document.getElementById("start");
 
 startButton.addEventListener('click', function () {
     // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ setInterval
-    timerId = setInterval(updateClock, 1000); // запускаем  updateClock() каждую секунду
+    if(!timerId){
+        timerId = setInterval(updateClock, 1000); // запускаем  updateClock() каждую секунду
+    }
+
+
 });
 
 
 const stopButton = document.getElementById('stop');
 stopButton.addEventListener('click', function () {
     // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ clearInterval
+
     clearInterval(timerId); // останавливаем таймер
+    timerId = null;
 });
 
 
